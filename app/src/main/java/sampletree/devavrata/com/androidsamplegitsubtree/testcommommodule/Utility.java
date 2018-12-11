d8320c4192c5359eb14a3bfb1fcd22268fdb0da6
DevavrataSharma <devavrata@techjini.com>
2018-12-11 18:42:01 +0530

    modified utility

---
+++ app/src/main/java/sampletree/devavrata/com/androidsamplegitsubtree/testcommommodule/Utility.java
@@ -0,0 +1,13 @@
+package sampletree.devavrata.com.androidsamplegitsubtree.testcommommodule;
+
+
+public class Utility {
+
+    public static String getName(){
+        return "DevavrataSharma_Spireon";
+    }
+
+}
+
+
+
